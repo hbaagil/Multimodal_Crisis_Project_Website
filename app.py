e162a1ea6762a8d7f53e085f0bc6dad0c57216a0
Hamzah Baagil <hhbaagil@gmail.com>
2023-09-12 15:05:32 +0200

    basic streamlit connected with the API

--- app.py
+++ app.py
@@ -5,44 +5,25 @@ import requests
 st.title("Crisis Helper 🚨")
 
 with st.form(key='params_for_api'):
+    tweet = st.text_input('What is your tweet?', '')
 
-    user_text = st.text_input('What is your tweet?','')
-    user_image = st.file_uploader('Upload an image', type=["jpg", "png", "jpeg"])
+    submitted = st.form_submit_button('Make prediction')
 
-    st.form_submit_button('Upload')
+# Check if the form was submitted
+if submitted:
+    params = dict(tweet=tweet)
+    api_url = 'https://crisishelper-qlahvylymq-ew.a.run.app/predict'
 
-if st.button("Show me the result"):
-    if not user_text and not user_image:
-        st.warning("Please enter text or upload an image.")
+    # Send a GET request to the external API
+    response = requests.get(api_url, params=params)
+
+    # Check if the API request was successful
+    if response.status_code == 200:
+        result = response.json()
+        st.success("Prediction Result:")
+
+        # Display the prediction result with improved formatting
+        for key, value in result.items():
+            st.write(f"**{key}:** {value}")
     else:
-        # Prepare the data to send to the API
-        data = {}
-        if user_text:
-            data["text"] = user_text
-
-        if user_image:
-            # Read and encode the image
-            image = Image.open(user_image)
-            image_bytes = io.BytesIO()
-            image.save(image_bytes, format="JPEG")
-            image_bytes = image_bytes.getvalue()
-
-            # Add the image data to the request
-            files = {"image": ("image.jpg", image_bytes)}
-
-params = dict(user_text=user_text, user_image=user_image)
-
-api_url = 'https:?????'
-response = requests.post(api_url, data=data, files=files if user_image else None)
-
-prediction = response.json()
-
-pred = prediction['label']
-
-# Display the API response
-if response.status_code == 200:
-            result = response.json()
-            st.success("Prediction Result:")
-            st.json(result)
-else:
-            st.error("Prediction failed. Please try again later.")
+        st.error("Prediction failed. Please try again later.")
